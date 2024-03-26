3e19e96b6c990b2ec8c1e9ddd36725d507fae862
Javier Trejo <javiertrejorod@gmail.com>
2024-03-25 19:45:35 -0600

    fix: re-design of Hero first view

--- menu.js
+++ menu.js
@@ -26,4 +26,25 @@
         $menu.classList.remove("is-active");
     });
 
+    // start carrousel
+   $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: false
+    });
+
+
+    // move next carousel
+    $('.moveNextCarousel').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('next');
+    });
+
+    // move prev carousel
+    $('.movePrevCarousel').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('prev');
+    });
+
 })(document);
